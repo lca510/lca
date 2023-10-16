6b8f8cb0b136adad5bc3e7c1ad60f8a6965cd852
lca510 <145953712+lca510@users.noreply.github.com>
2023-10-16 10:32:49 +0900

    Add files via upload

---
+++ 20231016.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+5%2
+
+
+# In[2]:
+
+
+5//2
+
+
+# In[3]:
+
+
+82+127
+
+
+# In[4]:
+
+
+25*24
+
+
+# In[5]:
+
+
+a = 2
+if a != 1:
+    print("1이 아님")
+
+
+# In[9]:
+
+
+a = 2
+a = a * 10
+a *= 10
+
+
+# In[8]:
+
+
+x = True
+y = False
+
+if x and y:
+    print("Yes")
+else:
+    print("No")
+
+
+# In[10]:
+
+
+a = [1,2,3,4]
+b = 3 in a
+print(b)
+
+
+# In[13]:
+
+
+a = "ABC"
+b = a
+print(a is b)
+
+
+# In[ ]:
+
+
+
+
